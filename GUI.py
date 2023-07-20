0c947a623f842787554a8c459b84a953649d7575
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-20 20:37:39 +0800

    Class and Designs added

--- GUI.py
+++ GUI.py
@@ -4,7 +4,13 @@ import csv
 from tkinter import ttk
 
 #Create a class for the GUI
+class ContactTracingApp:
 #Set the design for the app
+    def __init__(self):
+        self.app = tk.Tk()
+        self.app.title("COVID-19 Tracer by OhSnapItsAllen")
+        self.app.geometry("1280x720")
+        self.app.config(bg='light blue')
 #Add user inputs, gender will be a drop down menu  
 #Design the inputs and text display
 #Create a function to run the program
