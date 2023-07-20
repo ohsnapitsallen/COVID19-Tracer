4490dd9c546b4ab9e036de74e52572d14b308cb8
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-20 22:39:43 +0800

    Pseudocodes

---
+++ Contact Tracer.py
@@ -0,0 +1,6 @@
+#Import all necessary tools
+#Create a class that inherits the previous class
+#Add button function
+#Create an object for adding entries       
+#Create an object for searching entries (ADMIN PASSWORD REQUIRED)
+#Run the app
