4c88c85b2f41d32ccca4fea3bc09240d9e38d44c
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-20 23:39:58 +0800

    added getter for csv

--- Contact Tracer.py
+++ Contact Tracer.py
@@ -10,6 +10,12 @@ class ContactFunctions(ContactTracingApp):
         super().__init__()
         tk.Button(self.app, text='Add Entry', command=self.add_entry, bg='green', fg='white', font=('Arial', 20)).place(relx=0.4, y=720, anchor='center')
         tk.Button(self.app, text='Search Entry', command=self.search_entry, bg='blue', fg='white', font=('Arial', 20)).place(relx=0.6, y=720, anchor='center')
-#Create an object for adding entries       
+#Create an object for adding entries
+    def add_entry(self):
+        new_entry = [self.name.get(), self.email.get(), self.phone.get(), self.date.get(), self.gender.get(), self.recentcontacts.get()]
+        with open("contact_tracing.csv", "a", newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(new_entry)
+        messagebox.showinfo("Success", "Entry has been added successfully")      
 #Create an object for searching entries (ADMIN PASSWORD REQUIRED)
 #Run the app
