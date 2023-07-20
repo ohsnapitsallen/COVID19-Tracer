58995e7a6e4e03752bc155f8cab99051628ed7c4
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-20 19:53:35 +0800

    Pseudocodes

---
+++ GUI.py
@@ -0,0 +1,6 @@
+#Import all necessary tools for the GUI
+#Create a class for the GUI
+#Set the design for the app
+#Add user inputs, gender will be a drop down menu  
+#Design the inputs and text display
+#Create a function to run the program
