5a10a9092a1788ca6cbf13b36d102be6018f776c
John Allen Lorena <129713164+ohsnapitsallen@users.noreply.github.com>
2023-07-20 20:03:44 +0800

    Tools imported

--- GUI.py
+++ GUI.py
@@ -1,4 +1,8 @@
 #Import all necessary tools for the GUI
+import tkinter as tk
+import csv
+from tkinter import ttk
+
 #Create a class for the GUI
 #Set the design for the app
 #Add user inputs, gender will be a drop down menu  
